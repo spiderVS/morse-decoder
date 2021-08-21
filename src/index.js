c5dfb4544e6f0904ad76bc7934bac1973d2ed390
spiderVS <fuzzy003@gmail.com>
2021-08-21 12:22:35 +0300

    feat: add task solve

--- src/index.js
+++ src/index.js
@@ -38,7 +38,12 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+  let exprToArr = [];
+  for (let i = 0; i < expr.length; i += 10) {
+    if (expr.slice(i, i + 10) === '**********') exprToArr.push(' ');
+    else exprToArr.push(MORSE_TABLE[expr.slice(i, i + 10).replace(/00/g, '').replace(/10/g, '.').replace(/11/g, '-')]);
+  }
+  return exprToArr.join('');
 }
 
 module.exports = {
